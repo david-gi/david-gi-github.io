8a55afd07fa2ba8bcaa22a4d103ad937cbeb5732
David-Gi <DavidGi@gmx.com>
2021-04-13 17:34:10 -0400

    log response

--- code.js
+++ code.js
@@ -1,10 +1,12 @@
 try {
+  document.getElementById('Content').innerText = "Just a place for some of my dev thoughts and opinions to collect.\nUsually I type them out as they come on Notepad with or without saving them.\n\nI am and have been a full-stack web developer with affinity for the front-end."
   const href = window.location.href
   const url = href.substring(0, href.lastIndexOf('/'))
   const hash = Number(window.location.hash.substring(1)) || index
 
   const handleRes = () => {
     if (req.readyState == 4 && req.status == 200) {
+      console.log(req.responseText)
       const { title, content } = JSON.parse(req.responseText)
       document.getElementById('Title').innerText = title
       document.getElementById('Content').innerText = content
